543d86d4b4dddcf98c85d06d0fc814ad60f3e8f9
小牧 <89586586+xiaomu18@users.noreply.github.com>
2023-04-22 10:45:36 +0800

    Update SocketProtocol.py

--- SocketProtocol.py
+++ SocketProtocol.py
@@ -3,9 +3,10 @@ import struct
 class ExchangeProtocol:
     def __init__(self, sock) -> None:
         self.socket = sock
+        self.encoding = "utf-8"
 
     def send_msg(self, message: str):
-        message = message.encode("gbk")
+        message = message.encode(self.encoding)
         header = struct.pack('!I', len(message))  # 获取长度 Header
 
         self.socket.sendall(header + message)
@@ -14,7 +15,10 @@ class ExchangeProtocol:
         # 接收消息头
         header = self.socket.recv(4)
 
-        if len(header) != 4:
+        if header == b'':
+            raise ValueError("连接已断开")
+        
+        elif len(header) != 4:
             raise ValueError("Header 数据不完整. 无法解包.")
 
         message_len = struct.unpack('!I', header)[0]  # 解包消息长度
@@ -22,4 +26,4 @@ class ExchangeProtocol:
         # 接收消息内容
         data = self.socket.recv(message_len)
 
-        return data.decode("gbk")
+        return data.decode(self.encoding)
